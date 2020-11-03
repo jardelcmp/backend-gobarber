9db80b06f12eca46af9b7615af7cee4f4ff6913c
Jardel Costa <jardel.costa056@gmail.com>
2020-11-03 09:36:46 -0300

    adicionando cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import express, { json, Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import routes from './routes';
@@ -8,6 +9,7 @@ import AppError from './errors/AppError';
 import './database';
 
 const app = express();
+app.use(cors());
 app.use(json());
 app.use('/files', express.static(uploadConfig.directory));
 
